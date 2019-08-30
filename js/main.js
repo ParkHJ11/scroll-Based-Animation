ef0769b3105c1c48a931803e787eb1c795520607
phj <parkhj8801@gmail.com>
2019-08-31 02:38:32 +0900

    feature/스크롤이벤트 추가

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $(function(){
 
     var $device = $('.device');
     var $features = $('.features');
+    var $location = $('.location');
     var $offset = 500;
     var $deviceOST = $device.offset().top - $offset;
-    var $featuresOST = $('.features').offset().top - $offset;
+    var $featuresOST = $features.offset().top - $offset;
+    var $locationOST = $location.offset().top - $offset;
 
     $(window).scroll(function(){
         if($(this).scrollTop()>$deviceOST){
@@ -13,7 +15,17 @@ $(function(){
         if($(this).scrollTop()>$featuresOST){
             $features.find('img').addClass('grow-img');
             $features.find('h4').addClass('animate');
-        }
+        
+        if($(this).scrollTop()>$locationOST){
+            
+            
+            $('.stripe').each(function(idx){
 
+                var $this = $(this);
+
+                setTimeout(function(){ $this.addClass('animate_down');}, 100*idx);    
+
+            });
+        }    
     });
 });
